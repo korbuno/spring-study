faf8d017cccff19db646d15b7adbde7987a39adc
korbuno <aileen528@naver.com>
2018-07-18 01:20:32 +0900

    elastic beanstal에 적용중...

--- jpa/src/main/java/com/example/jpa/JpaApplication.java
+++ jpa/src/main/java/com/example/jpa/JpaApplication.java
@@ -1,12 +1,29 @@
 package com.example.jpa;
 
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan 
+@EnableAutoConfiguration 
 @SpringBootApplication
-public class JpaApplication {
+public class JpaApplication extends SpringBootServletInitializer {
+	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+		Server.createTcpServer(args).start();
 		SpringApplication.run(JpaApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JpaApplication.class);
+	}
+
 }
