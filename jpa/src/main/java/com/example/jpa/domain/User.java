fbcc437c950461dc33c220151ac0099c826fdbca
korbuno <aileen528@naver.com>
2018-07-17 21:17:16 +0900

    H2적용, JPA 엔티티 생성, 회원가입, 리스트 적용

--- jpa/src/main/java/com/example/jpa/domain/User.java
+++ jpa/src/main/java/com/example/jpa/domain/User.java
@@ -1,45 +1,69 @@
-package com.example.jpa.web;
+package com.example.jpa.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 
+	// Id : PK, GV : AutoInc
+	@Id @GeneratedValue
+	private long userNo;
+	
+	//nullable = false : null값 금지
+	//length = 20 : 길이 변경 (기본 255)
+	@Column(nullable=false, length=20)
 	private String userID;
 	private String userPass;
 	private String userName;
 	private String email;
-	
-	
-	
+
 	@Override
 	public String toString() {
 		return "User [userID=" + userID + ", userPass=" + userPass + ", userName=" + userName + ", email=" + email
 				+ "]";
 	}
+
+	public long getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(long userNo) {
+		this.userNo = userNo;
+	}
+
 	public String getUserID() {
 		return userID;
 	}
+
 	public void setUserID(String userID) {
 		this.userID = userID;
 	}
+
 	public String getUserPass() {
 		return userPass;
 	}
+
 	public void setUserPass(String userPass) {
 		this.userPass = userPass;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-	
-	
 
 }
--- jpa/src/main/java/com/example/jpa/web/UserController.java
+++ jpa/src/main/java/com/example/jpa/web/UserController.java
@@ -1,28 +1,33 @@
 package com.example.jpa.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.jpa.domain.User;
+import com.example.jpa.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	
-	private List<User> users = new ArrayList<User>();
-
-	@PostMapping("/create")
+	@Autowired
+	private UserRepository userRepository;
+	
+	@PostMapping("")
 	public String create(User user) {
-		users.add(user);
+		userRepository.save(user);
 		System.out.println(user);
-		return "redirect:/list";
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("")
 	public String list(Model model) {
-		model.addAttribute("users", users);
-		return "list";
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list";
 	}
+	
 }
